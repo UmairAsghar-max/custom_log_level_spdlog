ff1887dce11343679ab00b839e9cd6fbafdab483
UmairAsghar-max <umairasghar1042@gmail.com>
2023-02-22 23:30:08 +0500

    Custom_Features_Added

--- example/example.cpp
+++ example/example.cpp
@@ -21,16 +21,16 @@ int main() {
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
     auto logger = std::make_shared<spdlog::logger>("console", console_sink);
 
-    logger->trace("Hello, world! from TRACE {}",spdlog::level::trace);
-    logger->debug("Hello, world! from DEBUG {}",spdlog::level::debug);
-    logger->info("Hello, world! from INFO {}",spdlog::level::info);
-    logger->warn("Hello, world! from WARNING {}",spdlog::level::warn);
-    logger->hardwarefailure("Hello, world! from HARDWARE FAILURE {}",spdlog::level::hardwarefailure);
-    logger->error("Hello, world! from ERROR {}",spdlog::level::err);
-    logger->critical("Hello, world! from CRITICAL {}",spdlog::level::critical);
-    logger->fatal("Hello, world! from FATAL {}",spdlog::level::fatal);
-    logger->alert("Hello, world! from ALERT {}",spdlog::level::alert);
-    logger->emergency("Hello, world! from EMERGENCY {}",spdlog::level::emergency);
+    logger->trace("ECU-UUID: {} APP-UUID: {} SW-ID: LOG-LEVEL: {} ECU-TYPE: {} APP-TYPE: {} FEATURE: {} SECURITY: {}",2056,23456,1208,spdlog::level::trace,"ARM","TEST","WIFI","HIGH");
+    logger->debug("ECU-UUID: {} APP-UUID: {} SW-ID: LOG-LEVEL: {} ECU-TYPE: {} APP-TYPE: {} FEATURE: {} SECURITY: {}",2056,23456,1208,spdlog::level::debug,"ARM","TEST","WIFI","HIGH");
+    logger->info("ECU-UUID: {} APP-UUID: {} SW-ID: LOG-LEVEL: {} ECU-TYPE: {} APP-TYPE: {} FEATURE: {} SECURITY: {}",2056,23456,1208,spdlog::level::info,"ARM","TEST","WIFI","HIGH");
+    logger->warn("ECU-UUID: {} APP-UUID: {} SW-ID: LOG-LEVEL: {} ECU-TYPE: {} APP-TYPE: {} FEATURE: {} SECURITY: {}",2056,23456,1208,spdlog::level::warn,"ARM","TEST","WIFI","HIGH");
+    logger->hardwarefailure("ECU-UUID: {} APP-UUID: {} SW-ID: LOG-LEVEL: {} ECU-TYPE: {} APP-TYPE: {} FEATURE: {} SECURITY: {}",2056,23456,1208,spdlog::level::hardwarefailure,"ARM","TEST","WIFI","HIGH");
+    logger->error("ECU-UUID: {} APP-UUID: {} SW-ID: LOG-LEVEL: {} ECU-TYPE: {} APP-TYPE: {} FEATURE: {} SECURITY: {}",2056,23456,1208,spdlog::level::err,"ARM","TEST","WIFI","HIGH");
+    logger->critical("ECU-UUID: {} APP-UUID: {} SW-ID: LOG-LEVEL: {} ECU-TYPE: {} APP-TYPE: {} FEATURE: {} SECURITY: {}",2056,23456,1208,spdlog::level::critical,"ARM","TEST","WIFI","HIGH");
+    logger->fatal("ECU-UUID: {} APP-UUID: {} SW-ID: LOG-LEVEL: {} ECU-TYPE: {} APP-TYPE: {} FEATURE: {} SECURITY: {}",2056,23456,1208,spdlog::level::fatal,"ARM","TEST","WIFI","HIGH");
+    logger->alert("ECU-UUID: {} APP-UUID: {} SW-ID: LOG-LEVEL: {} ECU-TYPE: {} APP-TYPE: {} FEATURE: {} SECURITY: {}",2056,23456,1208,spdlog::level::alert,"ARM","TEST","WIFI","HIGH");
+    logger->emergency("ECU-UUID: {} APP-UUID: {} SW-ID: LOG-LEVEL: {} ECU-TYPE: {} APP-TYPE: {} FEATURE: {} SECURITY: {}",2056,23456,1208,spdlog::level::emergency,"ARM","TEST","WIFI","HIGH");
 
     return 0;
     
